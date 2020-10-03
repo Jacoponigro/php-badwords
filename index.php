172ea5d02b432392bf8bc51f3623e6248b1ca17d
Jacopo <jakonigro@gmail.com>
2020-10-03 15:59:21 +0200

    fine

---
+++ index.php
@@ -0,0 +1,20 @@
+/* dichiarazione variabili php e str_replace */
+<?php
+$bad_word = "jacopo";
+$paragraph ="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ipsum vero blanditiis, jacopo campione perspiciatis aliquid enim soluta assumenda vel cupiditate totam pariatur asperiores exercitationem beatae fuga animi labore nisi quibusdam.";
+$clean = str_replace($bad_word, "***", $paragraph);
+?>
+
+/* corpo html */
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>badword</title>
+  </head>
+  <body>
+    <p><?php echo $clean;?></p>
+  </body>
+</html>
